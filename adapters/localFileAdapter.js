e18391a75b4b995e3a1eb7fea14123351891f419
furenku <furenku@gmail.com>
2021-06-11 14:15:50 -0500

    cambiar config fileAdapter

--- adapters/localFileAdapter.js
+++ adapters/localFileAdapter.js
@@ -3,8 +3,7 @@ const { LocalFileAdapter } = require('@keystonejs/file-adapters');
 const { LOCAL_MEDIA_SERVER_FOLDER } = process.env
 
 const fileAdapter = new LocalFileAdapter({
-  // src: process.env.NODE_ENV === 'production' ? `./dist/${LOCAL_MEDIA_SERVER_FOLDER}` : `./${LOCAL_MEDIA_SERVER_FOLDER}`,
-  src: `./${LOCAL_MEDIA_SERVER_FOLDER}`,
+  src: process.env.NODE_ENV === 'production' ? `./dist/${LOCAL_MEDIA_SERVER_FOLDER}` : `./${LOCAL_MEDIA_SERVER_FOLDER}`,
   path: `/${LOCAL_MEDIA_SERVER_FOLDER}`,
 });
 
